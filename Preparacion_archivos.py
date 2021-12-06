11fc2c808f719a4214ec60ae7b541e0b129a339b
adrianpriet <61021661+adrianpriet@users.noreply.github.com>
2021-12-05 20:54:04 -0600

    Update Preparacion_archivos.py

--- Preparacion_archivos.py
+++ Preparacion_archivos.py
@@ -10,14 +10,28 @@ AUTHOR
         Adrian Prieto Castellanos <adrianpc@lcg.unam.mx>
 
 DESCRIPTION
-        Este programa prepara los archivos de modo  que se pueda realizar un analisis de
-        expresion diferencial a partir de archivos con cuentas crudas
+        Este programa cuenta con varias funciones que nos sirven para el 
+	correcto manejo y preparacion de archivos.
+	Función obtener_nombres: Con esta función obtenemos los nombres
+	que se encuentran en los archivos a traves de su ID.
+	Función obtener_longitudes: Con esta función obtenemos las longitudes
+	de los genes que se encuentra en nuestros archivos.
+	Funcion normalización: Con esta se normalizan el formato de todos los
+	archivos a su vez se obtienen los tpm(transcritos por millon).
+	
 
 CATEGORY
         Funcion
 
 INPUT
         Este programa recibe como input una serie de rutas a distintos archivos
+	'../data/e740e34f/e740e34f.tpm', '../data/e023c283/e023c283.tpm', '../data/d7bff2a7/d7bff2a7.tpm',
+        '../data/c71784ba/c71784ba.tpm', '../data/c592e62e/c592e62e.tpm', '../data/bb804a2f/bb804a2f.tpm',
+        '../data/b3f7a18b/b3f7a18b.tpm', '../data/a1e80ada/a1e80ada.tpm', '../data/97745843/97745843.tpm',
+        '../data/743ef32d/743ef32d.tpm', '../data/0e397288/0e397288.tpm', '../data/1e7b2b3b/1e7b2b3b.tpm',
+        '../data/40a5ed6b/40a5ed6b.tpm', '../data/03d7d46d/03d7d46d.tpm', '../data/4fd836c4/4fd836c4.tpm',
+        '../data/5d3c2256/5d3c2256.tpm', '../data/7cf82e1d/7cf82e1d.tpm', '../data/8b26a752/8b26a752.tpm',
+        '../data/8ddc8e8b/8ddc8e8b.tpm', '../data/39d6716f/39d6716f.tpm'
 
 GITHUB
 	https://github.com/rod13-afk/ProyectoFinal_PythonII/blob/master/Preparacion_archivos.py
