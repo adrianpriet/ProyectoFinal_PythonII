b6ff8e31b2d40953124fec7637380bcb817c8e9c
adrianpriet <61021661+adrianpriet@users.noreply.github.com>
2021-12-05 20:36:52 -0600

    Update Ttest.py

--- Ttest.py
+++ Ttest.py
@@ -3,24 +3,34 @@ NAME
         Ttest.py
 
 VERSION
-        1.0
+        2.0
 
 AUTHOR
         Victor Jesus Enriquez Castro  <victorec@lcg.unam.mx>
         Adrian Prieto Castellanos <adrianpc@lcg.unam.mx>
 
 DESCRIPTION
-        Este programa contiene una funcion que retorna una tabla con
-        los valores T y P, para cada par de archivos paciente:control
+        Este programa contiene una funcion que realiza un análisis
+        estadístico llamado T-test para evaluar si la diferencia
+        de expresión de las diferentes muestras es significativa
+        deacuerdo al grupo control.
 
 CATEGORY
-        Function
+        Función 
 
 INPUT
-        Este programa no recibe ningun input
+       La direccion de los archivos de las muestras como del control.
+       '../data/e740e34f/e740e34f.tpm', '../data/e023c283/e023c283.tpm', '../data/d7bff2a7/d7bff2a7.tpm',
+       '../data/c71784ba/c71784ba.tpm', '../data/c592e62e/c592e62e.tpm', '../data/bb804a2f/bb804a2f.tpm',
+       '../data/b3f7a18b/b3f7a18b.tpm', '../data/a1e80ada/a1e80ada.tpm', '../data/97745843/97745843.tpm',
+       '../data/743ef32d/743ef32d.tpm', '../data/0e397288/0e397288.tpm', '../data/1e7b2b3b/1e7b2b3b.tpm',
+       '../data/40a5ed6b/40a5ed6b.tpm', '../data/03d7d46d/03d7d46d.tpm', '../data/4fd836c4/4fd836c4.tpm',
+       '../data/5d3c2256/5d3c2256.tpm', '../data/7cf82e1d/7cf82e1d.tpm', '../data/8b26a752/8b26a752.tpm',
+       '../data/8ddc8e8b/8ddc8e8b.tpm', '../data/39d6716f/39d6716f.tpm'
+       ../data/Control.txt
 
 GITHUB
-        
+        https://github.com/rod13-afk/ProyectoFinal_PythonII/edit/master/Ttest.py
 '''
 
 #Se importan las librerias
