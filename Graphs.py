481d9ad9016a5b84fe5144d04646f3413f8269e2
rodan13 <rodrigohernandezb7@gmail.com>
2021-12-05 23:52:47 -0600

    Eliminacion de lineas innecesarias en Graphs

--- Graphs.py
+++ Graphs.py
@@ -307,8 +307,3 @@ def expression_each_gene():
         plt.title('Nivel de expresion del gen ' + str(random_genes[i]))
         plt.show()
         i += 1
-
-
-mayores_repeticiones()
-expresion_mayores_repeticiones()
-expression_each_gene()
